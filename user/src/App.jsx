7905f74a7ef88eef7b67ead8f43f4c2ef1cefee5
Lame-desu <lamesgindesalegn30@gmail.com>
2024-12-25 14:11:12 +0300

    Initial commit

---
+++ user/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Department from "./pages/Department/Department";
+import Contact from "./pages/Contact/Contact";
+import About from "./pages/About/About";
+import Auth from "./pages/Auth/Auth";
+
+function App() {
+  return (
+    <>
+  
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/department" element={<Department />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
